c50cffb56fc5bcd0963f848db93bf9eeaa139ec5
shibleNK <nomansible69@gmail.com>
2026-02-28 14:16:47 +0000

    correct structure

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
-
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
