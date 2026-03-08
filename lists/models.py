2ed95735f3f4f77eca06667bc275ecc259a2fef7
ShibleNK <nomansible69@gmail.com>
2026-03-08 05:44:52 +0000

    first steps of FT for layout + styling

--- lists/models.py
+++ lists/models.py
@@ -5,5 +5,5 @@ class List(models.Model):
     pass
 class Item(models.Model):
     text = models.TextField(default="")
-    list = models.ForeignKey(List , on_delete=models.CASCADE)
+    list = models.ForeignKey(List ,default=None, on_delete=models.CASCADE)
 
